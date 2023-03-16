f12eb3daee8a4b4059347dbea05c2ebdf01941e8
IdaDanuartha <idadanuartha42@smk.belajar.id>
2023-03-16 16:50:46 +0800

    tipe data python

--- 02-Variabel/main.py
+++ 02-Variabel/main.py
@@ -9,3 +9,12 @@ print("Nilai a = ", a, ", x = ", x, "dan panjang = ", panjang)
 # penamaan
 nilai_y = 15 # dengan menggunakan underscore
 juta10 = 10000000 # tidak boleh diawali dengan angka
+
+# pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+# assignment indirect
+b = a
+print("Nilai b = ", b)
