4bd44cbc5e925e8620925261df9b6432bd0863ff
IdaDanuartha <idadanuartha42@smk.belajar.id>
2023-03-16 16:39:10 +0800

    belajar cara kerja program dan variabel python

---
+++ 01-CaraKerjaProgram/main.py
@@ -0,0 +1,16 @@
+import time
+start_time = time.time()
+
+# Ini adalah komentar
+""" 
+Ini adalah
+Multi Line Comment
+"""
+print("Hello World")
+a = 5
+
+print(a)
+print(time.time() - start_time, "detik")
+
+# kita bisa mengcompile python ke bytecode
+# cara mengcompile, buka terminal dan tuliskan python -m py_compile main.py
---
+++ 02-Variabel/main.py
@@ -0,0 +1,11 @@
+# variabel adalah tempat menyimpan data
+
+a = 10
+x = 5
+panjang = 1000
+
+print("Nilai a = ", a, ", x = ", x, "dan panjang = ", panjang)
+
+# penamaan
+nilai_y = 15 # dengan menggunakan underscore
+juta10 = 10000000 # tidak boleh diawali dengan angka
