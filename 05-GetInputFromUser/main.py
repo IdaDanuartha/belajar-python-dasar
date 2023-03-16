df25924c04d00317988f45b6dadec9dd237c3629
IdaDanuartha <danuart21@gmail.com>
2023-03-16 19:53:19 +0800

    get input from user python

---
+++ 05-GetInputFromUser/main.py
@@ -0,0 +1,16 @@
+# Mengambil input dari user
+
+# Data yang dimasukkan pasti string
+data = input("Masukkan data: ")
+
+print("data = ", data, ", type = ", type(data))
+
+# Jika kita ingin mengambil int, maka
+num = int(input("Masukkan angka: "))
+
+print("data = ", num, ", type = ", type(num))
+
+# Bagaimana dengan boolean?
+biner = bool(input("Masukkan nilai boolean: "))
+
+print("data = ", biner, ", type = ", type(biner))
