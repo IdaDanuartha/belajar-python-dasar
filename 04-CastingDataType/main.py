d175cbc1d8c49c0c8d8bbef6c004c63b0530ad1b
IdaDanuartha <idadanuartha42@smk.belajar.id>
2023-03-16 18:26:44 +0800

    little update 2

---
+++ 04-CastingDataType/main.py
@@ -0,0 +1,15 @@
+# Part 4 kita akan belajar casting tipe data
+# yaitu merubah dari satu tipe ke tipe lain
+# tipe data = int, float, string, bool
+
+data_int = 1
+print("data : ", data_int, ", bertipe : ", type(data_int))
+
+data_float = float(data_int)
+print("data : ", data_float, ", bertipe : ", type(data_float))
+
+data_str = str(data_int)
+print("data : ", data_str, ", bertipe : ", type(data_str))
+
+data_bool = bool(data_int)
+print("data : ", data_bool, ", bertipe : ", type(data_bool))
