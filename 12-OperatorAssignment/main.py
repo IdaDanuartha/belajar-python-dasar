0363f65679f0e6bbbe812c476b556e5343639a11
IdaDanuartha <danuart21@gmail.com>
2023-03-17 12:50:09 +0800

    operator assignemnt

---
+++ 12-OperatorAssignment/main.py
@@ -0,0 +1,29 @@
+a = 5 # adalah assignment
+print('nilai a =', a)
+
+a += 1 # artinya adalah a = a + 1
+print('nilai a += 1, nilai a menjadi', a)
+
+a -= 2 # artinya adalah a = a + 2
+print('nilai a -= 2, nilai a menjadi', a)
+
+a *= 5 # artinya adalah a = a * 5
+print('nilai a *= 5, nilai a menjadi', a)
+
+a /= 2 # artinya adalah a = a / 2
+print('nilai a /= 2, nilai a menjadi', a)
+
+b = 10
+print('nilai b =', b)
+
+b %= 2 # artinya adalah b = b % 2
+print('nilai b %= 2, nilai b menjadi', b)
+
+b //= 2 # artinya adalah b = b // 2
+print('nilai b //= 2, nilai b menjadi', b)
+
+a = 5
+print('nilai a =', a)
+
+a **= 3 # artinya adalah a = a ** 1
+print('nilai a **= 1, nilai a menjadi', a)
