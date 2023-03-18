12068e6c68576fe9b7c827f1c69c406acdd16937
IdaDanuartha <danuart21@gmail.com>
2023-03-18 10:27:32 +0800

    latihan percabangan

---
+++ 19-ElifStatement/main.py
@@ -0,0 +1,12 @@
+# ELIF = else if statement
+
+nama = input('Nama kamu siapa? ')
+
+if nama == 'novel': # kondisi 1
+    print('Halo nopeell')
+elif nama == 'danu': # kondisi 2
+    print('Haiii danuu!')
+else :
+    print('Kamu siapaa? saya tidak kenall')
+
+print('Akhir dari program')
