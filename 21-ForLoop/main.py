fcd81b024f6f9fa027bcfca7af4ecc19a51d2d3e
IdaDanuartha <danuart21@gmail.com>
2023-03-18 20:38:56 +0800

    for loop

---
+++ 21-ForLoop/main.py
@@ -0,0 +1,18 @@
+# Perulangan (loop)
+
+# for kondisi:
+#   aksi...
+
+# dengan list
+angka = [0,1,2,3,4]
+
+for i in angka:
+    print(f"index ke = {i}")
+
+print("\n")
+
+# dengan range
+angka_range = range(1, 5)
+
+for i in angka_range:
+    print(f"index ke = {i}")
