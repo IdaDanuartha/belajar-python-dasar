4bd570c7add387a8199c25a425346dc3f1d1c034
IdaDanuartha <danuart21@gmail.com>
2023-03-19 10:59:42 +0800

    tuples and sets in python

---
+++ 33-TuplesSet/main.py
@@ -0,0 +1,21 @@
+# list
+
+# menggunakan kurung siku []
+data_list = [10,3,4,5,6]
+print(data_list)
+print(data_list[1])
+
+# tuples ()
+data_tuples = (10,3,4,5,6)
+print(data_tuples)
+print(data_tuples[1])
+
+# tidak bisa dilakukan assignment
+# data_tuples[1] = 99
+
+# sets {}
+data_sets = {10,3,4,5,6}
+print(data_sets)
+
+# tidak bisa
+# print(data_sets[0])
