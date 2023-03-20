f557e524c3979266dd20bc0a9f6798f3a2536b01
IdaDanuartha <danuart21@gmail.com>
2023-03-20 13:36:28 +0800

    import statement

---
+++ 38-ImportStatement/main.py
@@ -0,0 +1,12 @@
+# import fungsinya adalah untuk mengambil program dari file external .py
+
+import program_print
+
+# import dengan data
+import variable
+# data ada di namespace variable
+print(variable.data)
+
+# import dengan fungsi
+import matematika
+print(matematika.tambah(23,45))
