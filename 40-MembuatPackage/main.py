4d62c7a8a3f1514cd71677083d4e686ab8c63577
IdaDanuartha <danuart21@gmail.com>
2023-03-20 13:58:12 +0800

    make package in python

---
+++ 40-MembuatPackage/main.py
@@ -0,0 +1,8 @@
+import sains.matematika as math
+from sains import fisika
+
+hasil_tambah = math.tambah(4,25,2)
+print(f"Hasil tambah adalah {hasil_tambah}")
+
+gaya = fisika.gaya(90,10)
+print(f"Gaya adalah = {gaya}")
